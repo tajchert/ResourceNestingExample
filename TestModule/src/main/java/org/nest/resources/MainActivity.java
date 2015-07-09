266b416e50d242f0b9bc17a25c44b54306382be8
Michal Tajchert <mtajchert@gmail.com>
2015-07-10 00:28:53 +0200

    Yet this works...

--- TestModule/src/main/java/org/nest/resources/MainActivity.java
+++ TestModule/src/main/java/org/nest/resources/MainActivity.java
@@ -2,10 +2,19 @@ package org.nest.resources;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+
+    @Bind(R.id.someTextView)
+    TextView someTextView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_container);
+        ButterKnife.bind(this);
     }
 }
